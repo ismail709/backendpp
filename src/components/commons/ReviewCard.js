20563b991aa2492636b65b2fcb3e34c9f152d751
ismail709 <ismailrhilal@gmail.com>
2022-05-28 14:42:51 +0100

    minor changes 2

--- src/components/commons/ReviewCard.js
+++ src/components/commons/ReviewCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo } from "react";
 
 function ReviewCard({ name, stars, text }) {
   const starsEl = useMemo(() => {
