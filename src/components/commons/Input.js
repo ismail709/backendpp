66da14f95e6ea64828e7a2c1cd16575ae9c4723e
ismail709 <ismailrhilal@gmail.com>
2022-05-28 14:44:17 +0100

    minor changes 3

--- src/components/commons/Input.js
+++ src/components/commons/Input.js
@@ -5,7 +5,7 @@ function Input(props) {
     <>
       <label>{props.label}</label>
       <input
-        className="w-full text-xl outline-none focus:outline-[#3ea6ff] focus:outline-1 placeholder:italic placeholder:font-light border-[1px] border-gray-400"
+        className="p-1 w-full text-xl outline-none focus:outline-[#3ea6ff] focus:outline-1 placeholder:italic placeholder:font-light border-[1px] border-gray-400"
         {...props}
       />
     </>
